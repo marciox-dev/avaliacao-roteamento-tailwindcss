3cc50d02e6ed6eb0e79550aa779a9bfb0d881dca
Marcio <marcioxerfan@gmail.com>
2023-06-22 19:16:45 -0300

    Feat: added Hook useAuth

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,45 +1,12 @@
-import React, { createContext, useEffect, useState } from 'react'
-import { navigate, useNavigate } from 'react-router-dom';
+import React, { createContext } from 'react'
+import useAuth from '../hooks/useAuth';
 
 
 
 const AuthContext = React.createContext();
 
 const AuthProvider = ({ children }) => {
-  const [ userLogged, setUserLogged ] = useState(false);
-  const [ loading, setLoading ] = useState(true);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const userInfo = localStorage.getItem('userInfo')
-
-    if (userInfo) {
-      setUserLogged(true);
-    }
-    setLoading(false)
-  }, [])
-
-  const loginUser = async (inputValues) => {
-
-    const response = await fetch('http://localhost:3000/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(inputValues)
-    })
-    const data = await response.json();
-    console.log(data);
-    localStorage.setItem('userInfo', JSON.stringify(data))
-    navigate('/')
-    setUserLogged(true);
-  }
-
-  const logoutUser = () => {
-    setUserLogged(false);
-    localStorage.clear();
-    navigate('/login');
-  }
+  const {userLogged, loading, loginUser, logoutUser } = useAuth();
 
   if(loading){
     return <h>Loading</h>
